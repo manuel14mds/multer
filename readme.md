6d3d394a829938ec0f3f5f45033e5eece6c778f7
manuel14mds <manuel14mds@gmail.com>
2023-02-13 20:38:00 -0500

    add readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Multer image uploader
+_multer scaffolding_
+Send a formdata by post request.
+
+## Front:
+HTTP Form
+<img src="src/public/images/demo.gif" alt="demo" width="600"/>
+
+## Back:
+Endpoint: result request data view
+<img src="src/public/images/back" alt="demo" width="400"/>
--- src/public/scripts/validation.js
+++ src/public/scripts/validation.js
@@ -1,29 +1,3 @@
-
-/* (function () {
-    'use strict'
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    let forms = document.querySelectorAll('.needs-validation')
-    let error = false
-
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                    error = true
-                    console.log('algo fallo')
-                }
-                if(!error){
-                    console.log('no hay fallos')
-                }
-                form.classList.add('was-validated')
-            }, false)
-        })
-})() */
-
 function valuesValidator(form, event){
     let aux = true
     if (!form.checkValidity()) {
@@ -48,15 +22,4 @@ form.addEventListener('submit',(evt)=>{
         }).then(()=>window.location.href = "/")
 
     }
-    
-    /* const data = new FormData(form)
-    const obj = {}
-    data.forEach((value,key)=>obj[key]=value)
-    fetch('/api/sessions/register',{
-        method:'POST',
-        body:JSON.stringify(obj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(()=>window.location.href = "/login") */
 })
